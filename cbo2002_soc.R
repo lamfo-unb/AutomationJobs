d6eff59b3c228db44037ba7ebdb6b8651fb8f75f
RafaelEstatistico <rafa.unb.2012@gmail.com>
2018-02-23 14:48:24 -0300

    Adicionando Programacao

---
+++ cbo2002_soc.R
@@ -0,0 +1,34 @@
+library(dplyr)
+cbo2002_isco88 <- read.csv2("./Data/CBO2002_ISCO88.csv",
+                            colClasses = "character" ) %>% distinct(CBO2002, CIUO88)
+
+library(readxl)
+isco88_isco08 <- read_excel("./Data/ISCO88_ISCO08.xlsx",
+                            col_types = "text")
+
+isco08_soc <- read_excel("./Data/ISCO08_SOC.xls", 
+                         skip = 6, col_types = "text")
+
+Job_Zones <- read_excel("./Data/All_Job_Zones.xls", 
+                        skip = 3, col_types = "text")
+
+#---------------------------------------------------------------------------------------------------------
+auto <- Job_Zones %>%  mutate(Code = substr(Code,1,7)) %>% 
+        rename(Job_Zone = `Job Zone`) %>% distinct(Job_Zone, Code, Occupation)
+#---------------------------------------------------------------------------------------------------------
+
+library(dplyr)
+cbo2002_soc <- cbo2002_isco88 %>% left_join(isco88_isco08, by = c("CIUO88" = "ISCO-88 code")) %>%
+               distinct(CBO2002, `ISCO 08 Code`)
+cbo2002_soc <- cbo2002_soc %>% left_join(isco08_soc, by = c("ISCO 08 Code" = "ISCO-08 Code")) %>%
+               distinct(CBO2002, `2010 SOC Code`)
+cbo2002_soc <- cbo2002_soc %>% left_join(auto, by = c("2010 SOC Code" = "Code"))
+
+cbo2002_soc <- cbo2002_soc %>% distinct(CBO2002, Job_Zone) %>% na.omit()
+
+conta <- cbo2002_soc %>% group_by(CBO2002) %>% summarise(count = n())
+
+table(conta$count)
+
+write.csv2(cbo2002_soc, row.names = F, file = "./Data/CBO2002_SOC.csv")
+
---
+++ cbos_SELENIUM.R
@@ -0,0 +1,58 @@
+# Importando todos os CBOs
+cbo <- read.csv2("./Data/CBO94 - CBO2002 - Conversao com 90.csv", colClasses = c("character", "character") )
+
+cbo <- data.frame(CBO2002 = unique(cbo$CBO2002))
+
+cbo$cbo    <- NA
+cbo$CIUo88 <- NA
+cbo$titulo <- NA
+
+#------------------ Para processamento em multiplas maquinas ------------------------------------#
+cbo1 <- cbo[1:370,]
+cbo2 <- cbo[371:740,]
+cbo3 <- cbo[741:1100,]
+cbo4 <- cbo[1101:1480,]
+cbo5 <- cbo[1481:nrow(cbo),]
+#------------------------------------------------------------------------------------------------#
+
+# install.packages("RSelenium") 
+library(RSelenium)
+
+driver<- rsDriver()
+remDr <- driver[["client"]]
+
+remDr$navigate("http://www.mtecbo.gov.br/cbosite/pages/tabua/ConsultasConversao.jsf")
+
+optionelem1 <- remDr$findElement(using = "xpath", "//select[@id = 'formSite037:tabuaOrigem']/option[2]")
+optionelem1$clickElement()
+
+optionelem2 <- remDr$findElement(using = "xpath", "//select[@id = 'formSite037:tabuaDestino']/option[3]")
+optionelem2$clickElement()
+
+L <- 1
+for (i in cbo$CBO2002) {
+  print(L)
+  print(round(L/nrow(cbo), 3))
+optionelem <- remDr$findElement(using = "xpath", "//*[@id='formSite037']/div[2]/input[1]")
+
+optionelem$sendKeysToElement(list(paste("", i), key = "enter"))
+
+webElems <- remDr$findElements(using = "xpath", "//*[@id='formSite037:objeto:tbody_element']/tr")
+resHeaders <- unlist(lapply(webElems, function(x){x$getElementText()}))
+
+if(is.null(resHeaders)){
+  cbo[L, 3:5] <- NA
+} else cbo[L, 3:5] <-  unlist(strsplit(resHeaders, "\n"))
+
+Sys.sleep(runif(1, 3.723254, 6.5132689))
+
+L <- L+1
+
+}
+
+remDr$close()
+driver$server$stop()
+
+write.csv2(cbo, row.names = F,
+           file = "./Data/scrapy/cbo_remoto.csv")
+
