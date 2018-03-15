cff5e4d743cffe2f6e617a6b78520d803f2beba8
Pedro <pedro.melo.albuquerque@gmail.com>
2018-03-15 15:46:57 -0300

    Graphs Return

--- Analysis/Initial Graphs.R
+++ Analysis/Initial Graphs.R
@@ -2,70 +2,55 @@
 library(tidyverse)
 library(ggplot2)
 library(ineq)
-base1<-read.csv("Data\\automation_top1000.csv")
+library(PerformanceAnalytics)
+base1<-read.csv2("Data/jobzone_painel.csv")
 
-##############################################  Gráfico 1: #############################################################################
+##############################################  Graph 1 #############################################################################
 
-graph01.tbl1 <- base1 %>% 
-               group_by(ano) %>% 
-               summarise(total=sum(empregados))
-
-graph01.tbl2 <- base1 %>% group_by(ano,Job_Zone) %>% 
-               summarise(numerator=sum(empregados)) 
-              
+#Cumulative Growth Rate
+graph01.tbl1 <- base1 %>%
+  group_by(ano,Job_Zone) %>%
+  summarise(empreg = sum(empregados))
   
-graph01.tbl <- graph01.tbl1 %>% full_join(graph01.tbl2,by="ano") %>% 
-               mutate(freq=numerator/total,
-                      JobZone=as.character(Job_Zone)) 
-               
+graph01.tbl2 <- graph01.tbl1 %>%
+  group_by(Job_Zone) %>%
+  mutate(lag = lag(empreg)) %>%
+  mutate(pct.change = (empreg - lag) / lag)
+
+graph01.tbl2<-graph01.tbl2[!is.na(graph01.tbl2$pct.change),]
+
+graph01.tbl <- graph01.tbl2 %>%
+                group_by(Job_Zone) %>%
+                mutate(cumulative= cumprod(1 + pct.change) - 1)
+
 #pdf("GRAPH001.pdf")
-p<-ggplot(graph01.tbl, aes( x=ano, y=freq, colour=as.factor(JobZone), group=as.factor(JobZone) )) + 
+p<-ggplot(graph01.tbl, aes( x=ano, y=cumulative, colour=as.factor(Job_Zone), group=as.factor(Job_Zone) )) + 
   geom_line() + scale_colour_discrete(name="Automation",
-                                    breaks=c("2", "3", "4", "5"),
-                                    labels=c("Level 2", "Level 3", "Level 4", "Level 5")) +  xlab("Year")+ ylab("Frequency")
+                                      breaks=c("1","2", "3", "4", "5"),
+                                      labels=c("Level 1", "Level 2", "Level 3", "Level 4", "Level 5")) +  xlab("Year")+ ylab("Cumulative Growth Rate")
 
 p<-p+theme_bw()
 p
 #dev.off()
 
 
-##############################################  Gráfico 2: #############################################################################
+##############################################  Graph 2 #############################################################################
 
-graph02.tbl1 <- base1 %>% 
-  group_by(ano) %>% 
-  summarise(total=sum(renda))
-
-graph02.tbl2 <- base1 %>% group_by(ano,Job_Zone) %>% 
-  summarise(numerator=sum(renda)) 
-
-
-graph02.tbl <- graph02.tbl1 %>% full_join(graph02.tbl2,by="ano") %>% 
-  mutate(freq=numerator/total,
-         JobZone=as.character(Job_Zone)) 
+graph02.tbl<- graph01.tbl %>% 
+              group_by(Job_Zone) %>% 
+              mutate(
+                interpolated     = spline(x=ano, y=cumulative  , xout=ano)$y
+              )
 
 #pdf("GRAPH002.pdf")
-p<-ggplot(graph02.tbl, aes( x=ano, y=freq, colour=as.factor(JobZone), group=as.factor(JobZone) )) + 
-  geom_line() + scale_colour_discrete(name="Automation",
-                                      breaks=c("2", "3", "4", "5"),
-                                      labels=c("Level 2", "Level 3", "Level 4", "Level 5")) +  xlab("Year")+ ylab("Income")
 
+p<-ggplot(graph02.tbl, aes( x=ano, y=interpolated, colour=as.factor(Job_Zone), group=as.factor(Job_Zone) )) + 
+  geom_smooth(method = 'loess', span = .75, se = FALSE) + 
+  scale_colour_discrete(name="Automation",
+                                      breaks=c("1","2", "3", "4", "5"),
+                                      labels=c("Level 1", "Level 2", "Level 3", "Level 4", "Level 5")) +  xlab("Year")+ ylab("Cumulative Growth Rate")
 p<-p+theme_bw()
 p
 #dev.off()
 
-##############################################  Gráfico 3: #############################################################################
 
-graph03.tbl <- base1 %>% group_by(ano,Job_Zone) %>% 
-               summarise(gini.idx=ineq(renda)) %>% 
-               mutate(JobZone=as.character(Job_Zone)) 
-
-
-#pdf("GRAPH003.pdf")
-p<-ggplot(graph03.tbl, aes( x=ano, y=gini.idx, colour=as.factor(JobZone), group=as.factor(JobZone) )) + 
-  geom_line() + scale_colour_discrete(name="Automation",
-                                      breaks=c("2", "3", "4", "5"),
-                                      labels=c("Level 2", "Level 3", "Level 4", "Level 5")) +  xlab("Year")+ ylab("Gini Index")
-
-p<-p+theme_bw()
-p
-#dev.off()
