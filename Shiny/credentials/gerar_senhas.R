99d48ab888b9d13a179175d83bad1c3cc8d30d8f
Unknown <cayanportela@hotmail.com>
2018-07-11 13:35:10 -0300

    Atualizando credentials

---
+++ Shiny/credentials/gerar_senhas.R
@@ -0,0 +1,18 @@
+## Gerar Senhas ###
+library(dplyr)
+library(purrr)
+library(stringr)
+
+emails <- readRDS('~/AutomationJobs/R_scrap_selenium/emails_filtro2.rds')
+
+teste2 <- emails %>%
+          mutate( Nome_Espaco = map( Nomes , str_split , " "),
+                  Login_Senha = map( Nome_Espaco ,
+                                     ~  paste( str_sub( .x[[1]] , 1 , 2 ) , collapse = "" ) %>% 
+                                        iconv(to = "ASCII//TRANSLIT"))) 
+                              
+credentials2 <- data.frame(Nome = teste2[["Nomes"]] ,
+                           user = teste2[["Login_Senha"]] %>% unlist(), 
+                           pw   = teste2[["Login_Senha"]] %>% unlist())
+
+saveRDS(credentials2,'~/AutomationJobs/Shiny/credentials/credentials2.rds')
